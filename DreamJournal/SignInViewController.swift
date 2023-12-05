7dea95f39a7c969dedcbd05811ee6e249bcd0a3f
Eric Donnelly <200433532@student.georgianc.on.ca>
2023-12-05 18:35:41 -0500

    Added the widget and fixed a bug with signin

--- DreamJournal/SignInViewController.swift
+++ DreamJournal/SignInViewController.swift
@@ -66,8 +66,10 @@ class SignInViewController: UIViewController {
   
     
     @IBAction func LoginBtnPressed(_ sender: Any) {
+        // Check if both username and password are entered
         guard let username = usernameTextField.text, let password = passwordTextField.text, !username.isEmpty, !password.isEmpty else {
             print("Please enter both username and password.")
+            displayErrorMessage(message: "Please enter both username and password.") // Inform user to enter both fields
             return
         }
         
@@ -77,7 +79,7 @@ class SignInViewController: UIViewController {
           .getDocuments { (snapshot, error) in
             if let error = error {
                 print("Error getting documents: \(error)")
-                self.displayErrorMessage(message: "Error fetching user details")
+                self.displayErrorMessage(message: "Error fetching user details") // Inform user of error fetching details
             } else if let snapshot = snapshot, snapshot.documents.count > 0 {
                 // Assuming username is unique and fetches the first document
                 if let document = snapshot.documents.first,
@@ -86,21 +88,21 @@ class SignInViewController: UIViewController {
                     Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
                         if let error = error {
                             print("Login failed: \(error.localizedDescription)")
-                            self.displayErrorMessage(message: "Authentication Failed")
+                            self.displayErrorMessage(message: "Authentication Failed: \(error.localizedDescription)") // Inform user of failed authentication
                         } else {
                             print("User logged in successfully.")
                             DispatchQueue.main.async {
-                                self.performSegue(withIdentifier: "ToDashboard", sender: nil)
+                                self.performSegue(withIdentifier: "ToDashboard", sender: nil) // Segue to dashboard only on successful login
                             }
                         }
                     }
                 } else {
                     print("Email not found for the given username.")
-                    self.displayErrorMessage(message: "Invalid Username")
+                    self.displayErrorMessage(message: "Email not found for the given username") // Inform user if email for username not found
                 }
             } else {
                 print("Username does not exist.")
-                self.displayErrorMessage(message: "Invalid Username")
+                self.displayErrorMessage(message: "Username does not exist") // Inform user if username does not exist
             }
         }
     }
---
+++ DreamJournal_Widget/DreamJournal_Widget.swift
@@ -0,0 +1,87 @@
+import WidgetKit
+import SwiftUI
+import Intents
+
+
+struct Provider: IntentTimelineProvider {
+    var dreamsArray = ["Host dinner parties, Or start/join a supper club.","Steward a Little Free Library.","Take art classes.","Go away for a girls’ weekend.","Get really familiar with our local parks system.","I want to know the trails like the back of my hand."]
+
+
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), str: "Step 1", configuration: ConfigurationIntent())
+    }
+
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), str: "Step 2", configuration: configuration)
+        completion(entry)
+    }
+
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for timeOffset in 0 ..< dreamsArray.count {
+            let entryDate = Calendar.current.date(byAdding: .second, value: 2 * timeOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, str: dreamsArray[timeOffset], configuration: configuration)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let str: String
+    let configuration: ConfigurationIntent
+
+}
+
+struct DreamJournal_WidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text("DREAM OF THE DAY")
+            .font(.headline)// You can adjust the font style as needed
+            .padding(.top, 5)
+
+        VStack {
+            Text(entry.str)
+                .font(.system(size: 15))
+                .padding(.bottom, 4) // Add bottom padding for separation
+                .multilineTextAlignment(.center) // Set multiline text alignment
+                .lineLimit(nil) // Allow unlimited lines for the text
+
+            Image("Dream_journal_logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150, height: 80)
+        }
+        .padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12)) // Adjust overall padding
+    }
+        
+    
+    
+    
+}
+
+struct DreamJournal_Widget: Widget {
+    let kind: String = "DreamJournal_Widget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            DreamJournal_WidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Dream Journal Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct DreamJournal_Widget_Previews: PreviewProvider {
+    static var previews: some View {
+        DreamJournal_WidgetEntryView(entry: SimpleEntry(date: Date(), str: "Preview Text", configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
